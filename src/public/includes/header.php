0da19dc8732a12198cbd6122cc71d9c85f5d3126
clock-en <k.kuroda.biz@gmail.com>
2022-05-16 20:59:40 +0900

    トップページのデザイン作成

--- src/public/includes/header.php
+++ src/public/includes/header.php
@@ -1,6 +1,9 @@
 <header id="masthead" class="header">
   <h1 class="header__logo">Blogアプリ</h1>
-  <form method="post" action="/user/signout.php">
-    <button>ログアウト</button>
-  </form>
+  <div class="header__actions">
+    <a href="/user/signin.php">ログイン</a>
+    <form method="post" action="/user/signout.php">
+      <button>ログアウト</button>
+    </form>
+  </div>
 </header>
